74210648f7ff0da23aa7b2242efdd47040e59355
Mahamed Ibrahim <somalim4300@outlook.dk>
2024-11-29 11:47:11 +0100

    nav bar færdig

--- Github/Program.cs
+++ Github/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, kebab!");
+
+Console.WriteLine("Anton har skrevet her");
+Console.WriteLine(" ");
+
+Console.WriteLine("har lavet en nav bar!!");
